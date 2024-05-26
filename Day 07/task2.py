244ce3d74a7e3c9ff65d709f03ad38cba8628178
wiklaur1 <wiklaurigiorgi192@gmail.com>
2024-05-27 02:40:27 +0400

    commit

---
+++ Day 07/task2.py
@@ -0,0 +1,19 @@
+"""3) შექმენით პროგრამა სადაც მომხმარებელი შემოიტანს 5 რიცხვს, 
+ხოლო ამ 5 რიცხვს შორის გამოიყენეთ ყველა არითმეტიკული ოპერაცია (// და %_იც), 
+საბოლოოდ დაბეჭდეთ შედეგები ტერმინალში + ახსენით თითოეული ნაწილი კოდის რატომ დაწერეთ კონკრეტული ხაზი და რას აკეთებს."""
+
+
+number1 = int(input("please enter number1: ")) #declared varieble as integer converted input value
+number2 = int(input("please enter number2: "))
+number3 = int(input("please enter number3: "))
+number4 = int(input("please enter number4: "))
+number5 = int(input("please enter number5: "))
+number6 = int(input("please enter number6: ")) 
+number7 = int(input("please enter number7: "))
+
+
+print(number1 + number2)#added number1 and number2 
+print(number2 * number3)#multiplied number2 and number3
+print(number4 // number5)#floordivided number4 by number5
+print(number5 % number6)#modulo number5 by number6
+print(number6 - number7)#subtracted number7 from number6
