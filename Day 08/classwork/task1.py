8ff8364f1c2ce732736d179146352a5655515e74
wiklaur1 <wiklaurigiorgi192@gmail.com>
2024-05-28 01:37:43 +0400

    commit

---
+++ Day 08/classwork/task1.py
@@ -0,0 +1,12 @@
+# >, <, ==, >=, <=, !=
+
+print(5 > 10)
+print(12 < 14)
+print(10 == 10)
+print(12 >= 12)
+print(1 <= 14)
+print(15 != 15)
+print(15 == 24) 
+print(13 >= 15)
+print(13 <= 6)
+print(10>7)
---
+++ Day 08/homework/task2.py
@@ -0,0 +1,29 @@
+"""3) ჩამოწერეთ 10-10 მაგალითი and და or ოპერატორებზე"""
+
+
+# or - ან 
+
+print(5==5 or 99<12)
+print(12<=11 or 76!=22)
+print(9>15 or 12>=91)
+print(5>6 or 77==77)
+print(12!=19 or 12==99)
+print(54<21 or 45==12)
+print(88>=12 or 35!=22)
+print(61==45 or 12<11)
+print(19<=22 or 19==19)
+print(31>12 or 65!=12)
+
+
+# and - და
+
+print(14==77 and 12>99)
+print(0>12 and 66!=12)
+print(99>45 and 34<=34)
+print(10>24 and 87>=11)
+print(67==54 and 55!=67)
+print(0>-1 and 35<12)
+print(88>12 and 12==12)
+print(56!=54 and 66==66)
+print(94>54 and 12<0)
+print(99==100 and 56!=97)
