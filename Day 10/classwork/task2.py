4cabc6c7dc576c6f8603de6e311cb849494f766d
wiklaur1 <wiklaurigiorgi192@gmail.com>
2024-06-09 15:24:40 +0400

    commit

--- Day 10/classwork/task2.py
+++ Day 10/classwork/task2.py
@@ -3,4 +3,10 @@
 ხოლო სხვა შემთხვევაში დაბეჭდეთ მეორე რიცხვი"""
 
 
-# ?
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
