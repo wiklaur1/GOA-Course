6adacb8a6df328dc4c61f5f73d5335ecb9519462
wiklaur1 <wiklaurigiorgi192@gmail.com>
2024-06-25 20:20:08 +0400

    commit

---
+++ Day 16/homework/task2.py
@@ -0,0 +1,11 @@
+"""2) https://www.w3schools.com/python/python_lists.asp გადახედეთ ამ მასალას და თითო სექციაზე გააკეთეთ 5-5 მაგალითი"""
+
+
+
+Cars = ["BMW","Mercedes","Pagani"]
+print(Cars)
+
+Fruit = ["Apple","Banana","Orange"]
+print(Fruit)
+
+
